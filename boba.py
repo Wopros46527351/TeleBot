614c86f7fd927250c6794d8131b865c39c832c38
Wopros46527351 <77889318+Wopros46527351@users.noreply.github.com>
2021-08-19 14:19:44 +0300

    up

--- boba.py
+++ boba.py
@@ -1,7 +1,5 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import os
-PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -49,10 +47,7 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://gentle-scrubland-84131.herokuapp.com/' + TOKEN)
+    updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
